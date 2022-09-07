fcbe64a0b0fd775c374b44e656ec5fb6da394168
Unity Technologies <@unity>
2022-09-07 00:00:00 +0000

    com.unity.formats.fbx@5.0.0-pre.1 ## [5.0.0-pre.1] - 2022-09-07
    ### Added
    - Exposed FBX Recorder Settings class to the public API.
    - Exposed functions to pass export options on export to the public API.
    - Exposed function to convert to prefab variant to the public API.
    
    ### Changed
    - Added support for exporting texture offset, scale and wrap mode. Thank you to @hybridherbst for the additions.
    - Updated the minimum Unity version to 2020.3.
    - Updated the minimum Recorder version to 4.0.0.
    - Updated the FBX SDK bindings to 5.0.0-pre.1.
    
    ### Fixed
    - Cache result of SelectionContainsPrefabInstance in Editor Window to avoid unnecessary recalculations.
    - Use lists instead of dictionaries for property channel maps in order to ensure consistent order of operations.
    - Add optimizations to speed up convert to prefab variant.
    - Improve formatting and functions of the FBX exporter. Thank you to @smitdylan2001 for the contributions.
    - Fix errors when opening FBX export project settings on Linux.

--- Editor/Sources/Recorders/FbxRecorder/FbxRecorder.cs
+++ Editor/Sources/Recorders/FbxRecorder/FbxRecorder.cs
@@ -1,10 +1,8 @@
-﻿#if ENABLE_FBX_RECORDER
-using System.Collections;
+#if ENABLE_FBX_RECORDER
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Recorder;
 using UnityEditor.Recorder.Input;
-using UnityEditor;
 
 namespace UnityEditor.Formats.Fbx.Exporter
 {
@@ -12,21 +10,14 @@ namespace UnityEditor.Formats.Fbx.Exporter
     {
         protected override void RecordFrame(RecordingSession ctx)
         {
-
         }
 
-        protected override void EndRecording(RecordingSession session)
+        private void EndRecordingInternal(RecordingSession session)
         {
-            if(session == null)
-            {
-                throw new System.ArgumentNullException("session");
-            }
-
             var settings = (FbxRecorderSettings)session.settings;
 
             foreach (var input in m_Inputs)
             {
-
                 var aInput = (AnimationInput)input;
 
                 if (aInput.GameObjectRecorder == null)
@@ -38,28 +29,32 @@ namespace UnityEditor.Formats.Fbx.Exporter
 
                 var absolutePath = FileNameGenerator.SanitizePath(settings.FileNameGenerator.BuildAbsolutePath(session));
                 var clipName = absolutePath.Replace(FileNameGenerator.SanitizePath(Application.dataPath), "Assets");
-                
+
 #if UNITY_2019_3_OR_NEWER
-                var options = new Animations.CurveFilterOptions();
-                options.keyframeReduction = false;
+                var options = settings.GetCurveFilterOptions(settings.AnimationInputSettings.SimplyCurves);
                 aInput.GameObjectRecorder.SaveToClip(clip, settings.FrameRate, options);
 #else
-                aInput.GameObjectRecorder.SaveToClip(clip, settings.FrameRate);
+                    aInput.GameObjectRecorder.SaveToClip(clip, settings.FrameRate);
 #endif
+                if (settings.AnimationInputSettings.ClampedTangents)
+                {
+                    FilterClip(clip);
+                }
+
                 var root = ((AnimationInputSettings)aInput.settings).gameObject;
                 clip.name = "recorded_clip";
 
                 var exportSettings = new ExportModelSettingsSerialize();
                 exportSettings.SetAnimationSource(settings.TransferAnimationSource);
                 exportSettings.SetAnimationDest(settings.TransferAnimationDest);
-                exportSettings.SetObjectPosition(ExportSettings.ObjectPosition.WorldAbsolute);
+                exportSettings.SetObjectPosition(ObjectPosition.WorldAbsolute);
                 // export animated skinned meshes so that blendshape animation will export
                 exportSettings.SetAnimatedSkinnedMesh(true);
-                var toInclude = ExportSettings.Include.ModelAndAnim;
+                var toInclude = Include.ModelAndAnim;
                 if (!settings.ExportGeometry)
                 {
-                    toInclude = ExportSettings.Include.Anim;
-                } 
+                    toInclude = Include.Anim;
+                }
                 exportSettings.SetModelAnimIncludeOption(toInclude);
 
                 var exportData = new AnimationOnlyExportData();
@@ -71,7 +66,34 @@ namespace UnityEditor.Formats.Fbx.Exporter
 
                 aInput.GameObjectRecorder.ResetRecording();
             }
+        }
+
+        protected override void EndRecording(RecordingSession session)
+        {
+            if (session == null)
+            {
+                throw new System.ArgumentNullException("session");
+            }
+
+            if (Recording)
+            {
+                EndRecordingInternal(session);
+            }
             base.EndRecording(session);
+        }
+
+        void FilterClip(AnimationClip clip)
+        {
+            foreach (var bind in AnimationUtility.GetCurveBindings(clip))
+            {
+                var curve = AnimationUtility.GetEditorCurve(clip, bind);
+                for (var i = 0; i < curve.keys.Length; ++i)
+                {
+                    AnimationUtility.SetKeyLeftTangentMode(curve, i, AnimationUtility.TangentMode.ClampedAuto);
+                    AnimationUtility.SetKeyRightTangentMode(curve, i, AnimationUtility.TangentMode.ClampedAuto);
+                }
+                AnimationUtility.SetEditorCurve(clip, bind, curve);
+            }
         }
     }
 }
